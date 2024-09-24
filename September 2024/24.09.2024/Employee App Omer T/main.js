f47702bad68daec41f2ab498c714755fe7c60888
Yosef Hayim <yosefisabag+03@gmail.com>
2024-09-24 22:37:19 +0300

    fsd

--- September 2024/24.09.2024/Employee App Omer T/main.js
+++ September 2024/24.09.2024/Employee App Omer T/main.js
@@ -75,23 +75,23 @@ function displayAllEmployees() {
   for (let i = 0; i < employees.length; i++) {
     const employee = employees[i];
 
-    const listItemEl = document.createElement("li");
-    listItemEl.classList.add(`${generateId()}`);
+    const employeeDataLI = document.createElement("li");
+    employeeDataLI.classList.add(`${generateId()}`);
 
-    listItemEl.innerHTML = `
-      <p>First Name: ${employee.firstName}</p>
-      <p>Last Name: ${employee.lastName}</p>
-      <p>Age: ${employee.age}</p>
-      <p>Start Date: ${employee.startDate}</p>
-      <p>Department: ${employee.department}</p>
-      <p>Salary: ${employee.salary}</p>
+    employeeDataLI.innerHTML = `
+      <p class="fName">First Name: ${employee.firstName}</p>
+      <p class="lName">Last Name: ${employee.lastName}</p>
+      <p class="age">Age: ${employee.age}</p>
+      <p class="s-date">Start Date: ${employee.startDate}</p>
+      <p class="department">Department: ${employee.department}</p>
+      <p class="salary">Salary: ${employee.salary}</p>
       <div>
         <button class="delete-button">Delete</button>
         <button class="edit-button">Edit</button>
         <button class="update-button">Update</button>
       </div>
     `;
-    employeeListEl.append(listItemEl);
+    employeeListEl.append(employeeDataLI);
   }
 }
 
@@ -101,29 +101,43 @@ function addNewEmployee() {
   formEl.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const firstName = document.querySelector("#fname").value;
-    const lastName = document.querySelector("#lname").value;
-    const age = document.querySelector("#age").value;
-    const startDate = document.querySelector("#start-date").value;
-    const department = document.querySelector("#department-employees").value;
-    const salary = document.querySelector("#salary").value;
+    const firstNameInputValue = document.querySelector("#fname").value;
+    const lastNameInputValue = document.querySelector("#lname").value;
+    const ageInputValue = document.querySelector("#age").value;
+    const startDateInputValue = document.querySelector("#start-date").value;
+    const departmentInputValue = document.querySelector(
+      "#department-employees"
+    ).value;
+    const salaryInputValue = document.querySelector("#salary").value;
+
+    const newEmployee = {
+      firstName: firstNameInputValue,
+      lastName: lastNameInputValue,
+      age: ageInputValue,
+      startDate: startDateInputValue,
+      department: departmentInputValue,
+      salary: salaryInputValue,
+    };
+
+    employees.push(newEmployee);
 
     const newEmployeeEl = document.createElement("li");
     newEmployeeEl.classList.add(`${generateId()}`);
 
     newEmployeeEl.innerHTML = `
-      <p>First Name: ${firstName}</p>
-      <p>Last Name: ${lastName}</p>
-      <p>Age: ${age}</p>
-      <p>Start Date: ${startDate}</p>
-      <p>Department: ${department}</p>
-      <p>Salary: ${salary}</p>
+      <p class="fName">First Name: ${firstNameInputValue}</p>
+      <p class="lName">Last Name: ${lastNameInputValue}</p>
+      <p class="age">Age: ${ageInputValue}</p>
+      <p class="s-date">Start Date: ${startDateInputValue}</p>
+      <p class="department">Department: ${departmentInputValue}</p>
+      <p class="salary">Salary: ${salaryInputValue}</p>
       <div>
         <button class="delete-button">Delete</button>
         <button class="edit-button">Edit</button>
         <button class="update-button">Update</button>
       </div>
     `;
+
     employeeListEl.append(newEmployeeEl);
   });
 }
@@ -150,26 +164,25 @@ function filterByDepartment() {
       displayAllEmployees();
     } else {
       for (let i = 0; i < employees.length; i++) {
-        const employeeDepartment = employees[i].department.toLowerCase();
+        const employee = employees[i];
+        const employeeDepartment = employee.department.toLowerCase();
 
         if (employeeDepartment === selectedDepartment) {
-          const eachEmployee = employees[i];
-
           const filteredEmployeeEl = document.createElement("li");
           filteredEmployeeEl.classList.add(`${generateId()}`);
 
           filteredEmployeeEl.innerHTML = `
-            <p>First Name: ${eachEmployee.firstName}</p>
-            <p>Last Name: ${eachEmployee.lastName}</p>
-            <p>Age: ${eachEmployee.age}</p>
-            <p>Start Date: ${eachEmployee.startDate}</p>
-            <p>Department: ${eachEmployee.department}</p>
-            <p>Salary: ${eachEmployee.salary}</p>
-            <div>
-              <button class="delete-button">Delete</button>
-              <button class="edit-button">Edit</button>
-              <button class="update-button">Update</button>
-            </div>
+        <p class="fName">First Name: ${employee.firstName}</p>
+        <p class="lName">Last Name: ${employee.lastName}</p>
+        <p class="age">Age: ${employee.age}</p>
+        <p class="s-date">Start Date: ${employee.startDate}</p>
+        <p class="department">Department: ${employee.department}</p>
+        <p class="salary">Salary: ${employee.salary}</p>
+        <div>
+          <button class="delete-button">Delete</button>
+          <button class="edit-button">Edit</button>
+          <button class="update-button">Update</button>
+        </div>
           `;
           employeeListEl.append(filteredEmployeeEl);
         }
@@ -178,7 +191,52 @@ function filterByDepartment() {
   });
 }
 
+function editCurrentEmployee() {
+  employeeListEl.addEventListener("click", function (event) {
+    if (event.target.classList.contains("edit-button")) {
+      event.preventDefault();
+
+      const currentEmployeeData = event.target.closest("li");
+
+      const fName = currentEmployeeData.querySelector(".fName");
+      const lName = currentEmployeeData.querySelector(".lName");
+      const age = currentEmployeeData.querySelector(".age");
+      const startDate = currentEmployeeData.querySelector(".s-date");
+      const department = currentEmployeeData.querySelector(".department");
+      const salary = currentEmployeeData.querySelector(".salary");
+
+      fName.contentEditable = "true";
+      lName.contentEditable = "true";
+      age.contentEditable = "true";
+      startDate.contentEditable = "true";
+      department.contentEditable = "true";
+      salary.contentEditable = "true";
+
+      currentEmployeeData.style.backgroundColor = "#f0f0f0";
+    }
+
+    if (event.target.classList.contains("update-button")) {
+      event.preventDefault();
+
+      fName.textContent = ` ${fName.textContent}`;
+      lName.textContent = `${lName.textContent}`;
+      age.textContent = `${age.textContent}`;
+      startDate.textContent = `${startDate.textContent}`;
+      department.textContent = `${department.textContent}`;
+      salary.textContent = `${salary.textContent}`;
+
+      fName.contentEditable = "false";
+      lName.contentEditable = "false";
+      age.contentEditable = "false";
+      startDate.contentEditable = "false";
+      department.contentEditable = "false";
+      salary.contentEditable = "false";
+    }
+  });
+}
+
 displayAllEmployees();
 addNewEmployee();
 removeEmployee();
 filterByDepartment();
+editCurrentEmployee();

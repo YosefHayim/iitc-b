2c1337e8d4baffea39f19937b8716da2f5d81ea8
Yosef Hayim <yosefisabag+03@gmail.com>
2024-10-08 21:27:33 +0300

    fdsfsd

--- October 2024/08.10.2024/fitoussi-t-frontend-mentor/main.js
+++ October 2024/08.10.2024/fitoussi-t-frontend-mentor/main.js
@@ -7,14 +7,12 @@ btns.forEach((button) => {
     const btnImage = button.querySelector('.icon-plus');
     const pElement = button.closest('.question-container').querySelector('p');
 
-    // Toggle the 'hidden' class on <p>
     pElement.classList.toggle('hidden');
 
-    // Update the image source based on the visibility of the <p>
     if (pElement.classList.contains('hidden')) {
-      btnImage.src = './assets/images/icon-plus.svg'; // Show "plus" icon when text is hidden
+      btnImage.src = './assets/images/icon-plus.svg'; 
     } else {
-      btnImage.src = './assets/images/icon-minus.svg'; // Show "minus" icon when text is visible
+      btnImage.src = './assets/images/icon-minus.svg'; 
     }
   });
 });

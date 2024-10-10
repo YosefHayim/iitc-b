fe0ea363fe69acacc2fcbe251e57c3d2b90eb69b
Yosef Hayim <yosefisabag+03@gmail.com>
2024-10-10 18:44:45 +0300

    finish fitoussi t

---
+++ October 2024/10.10.2024/fitoussi-t-openweatherapi/env.js
@@ -0,0 +1,3 @@
+let apiKey = `faf110a59fa592f3bb08d8544ea30081`
+
+export {apiKey}
--- October 2024/10.10.2024/fitoussi-t-openweatherapi/main.js
+++ October 2024/10.10.2024/fitoussi-t-openweatherapi/main.js
@@ -1,3 +1,6 @@
-fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${api_key}`)
+import { apiKey } from "./env.js";
+
+fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`)
 .then((result) => console.log(result))
+.then((data) => console.log(data))
 .catch((error) => console.error(error))

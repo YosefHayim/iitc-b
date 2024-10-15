bf93dbc3bad7c34390f88baee257101010e25ec7
Yosef Hayim <yosefisabag+03@gmail.com>
2024-10-15 22:06:47 +0300

    fds

--- October 2024/14.10.2024/Sukot assigment/functionalities/main.js
+++ October 2024/14.10.2024/Sukot assigment/functionalities/main.js
@@ -43,12 +43,34 @@ const postData = async (url,cb) => {
 const popularMovies = () => {
   getData(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${apiKey}`, (data) => {
     data.results.forEach((movie) => {
+
+      // Determine the correct star rating image based on vote_average
+      let starRatingImage;
+      if (movie.vote_average < 2) {
+        starRatingImage = "../images/one-star-icon.svg";
+      } else if (movie.vote_average < 4) {
+        starRatingImage = "../images/two-stars-icon.svg";
+      } else if (movie.vote_average < 6) {
+        starRatingImage = "../images/three-stars-icon.svg";
+      } else if (movie.vote_average < 8) {
+        starRatingImage = "../images/four-stars-icon.svg";
+      } else {
+        starRatingImage = "../images/five-stars-icon.svg";
+      }
+
       popularMoviesContainer.innerHTML += `
         <div class="movie-card">
-        <h1 class="title">${movie.original_title}</h1>
-        <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="movie-img" class="movie-img">
-        <p class="overview">${movie.overview}</p>
-        <p class="score">Rate:${movie.vote_average}</p>
+          <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="movie-img" class="movie-img">
+          <h1 class="title">${movie.original_title}</h1>
+
+          <div class="img-container">
+          <img src="${starRatingImage}" alt="rating-img" class="rating-img">
+          <h2 class="rating-number-txt">${movie.vote_average.toFixed(1)}</h2>
+          </div>
+
+          <div class="overview-container">
+            <p class="overview">${movie.overview}</p>
+          </div>
         </div>
       `;
     });
@@ -61,17 +83,40 @@ const popularMovies = () => {
 const topRatedMovies = () => {
   getData(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${apiKey}`, (data) => {
     data.results.forEach((movie) => {
+
+      // Determine the correct star rating image based on vote_average
+      let starRatingImage;
+      if (movie.vote_average < 2) {
+        starRatingImage = "../images/one-star-icon.svg";
+      } else if (movie.vote_average < 4) {
+        starRatingImage = "../images/two-stars-icon.svg";
+      } else if (movie.vote_average < 6) {
+        starRatingImage = "../images/three-stars-icon.svg";
+      } else if (movie.vote_average < 8) {
+        starRatingImage = "../images/four-stars-icon.svg";
+      } else {
+        starRatingImage = "../images/five-stars-icon.svg";
+      }
+
       topRatedMoviesContainer.innerHTML += `
         <div class="movie-card">
-        <h1 class="title">${movie.original_title}</h1>
-        <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="movie-img" class="movie-img">
-        <p class="overview">${movie.overview}</p>
-        <p class="score">Rate:${movie.vote_average}</p>
+          <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="movie-img" class="movie-img">
+          <h1 class="title">${movie.original_title}</h1>
+
+          <div class="img-container">
+          <img src="${starRatingImage}" alt="rating-img" class="rating-img">
+          <h2 class="rating-number-txt">${movie.vote_average.toFixed(1)}</h2>
+          
+          </div>
+          <div class="overview-container">
+            <p class="overview">${movie.overview}</p>
+          </div>
         </div>
       `;
     });
   });
 };
+
 
 popularMovies();
 topRatedMovies();

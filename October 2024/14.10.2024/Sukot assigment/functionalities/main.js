e02ca53cd82502796f5ba623fc705061a92a064b
YosefHayim <yosefisabag+03@gmail.com>
2024-10-15 15:28:26 +0300

    dsa

--- October 2024/14.10.2024/Sukot assigment/functionalities/main.js
+++ October 2024/14.10.2024/Sukot assigment/functionalities/main.js
@@ -18,8 +18,26 @@ const popularMovies = async () => {
     <div class="movie-card-container">
     <div class="movie-title-container"><h2>${movie.original_title}</h2></div>
 
-    <div class="img-container"><img src="https://image.tmdb.org/t/p/w500/${movie.backdrop_path}" alt="movie-poster-img" class="movie-poster-img"> </div>
+    <div class="img-container"><img src="https://image.tmdb.org/t/p/original/${movie.backdrop_path}" alt="movie-poster-img" class="movie-poster-img"> </div>
     <div class="p-container"><p>${movie.overview}</p>
+    <div class="buttons-container">
+
+    <div class="like-container">
+      <button class="like-button"><img src="../images/reg-heart-icon.png" alt="reg-heart-icon" class="reg-heart-icon"></button>
+      <p>Like</p>
+    </div>
+
+    <div class="share-container">
+      <button class="like-button"><img src="../images/share-icon.png" alt="share-icon" class="reg-share-icon"></button>
+      <p>Share</p>
+    </div>
+
+    <div class="rating-container">
+      <button class="like-button"><img src="../images/rate-icon.png" alt="rate-icon" class="reg-rate-icon"></button>
+      <p>Rating</p>
+    </div>
+
+    </div>
     </div>
     `    
   });

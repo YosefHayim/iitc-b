9d1688b2f33d32fb93492ec5a2cb4dea63efedcc
Yosef Hayim <yosefisabag+03@gmail.com>
2024-10-15 21:32:51 +0300

    fds

---
+++ October 2024/14.10.2024/Sukot assigment/README.md
@@ -0,0 +1,2 @@
+inspiration for app
+https://dribbble.com/shots/3582624-ticket-recommend/attachments/799825?mode=media
--- October 2024/14.10.2024/Sukot assigment/functionalities/main.js
+++ October 2024/14.10.2024/Sukot assigment/functionalities/main.js
@@ -1,6 +1,9 @@
 import { apiKey, apiToken } from "./env.js";
 
-const getData = async (url, callback) => {
+const popularMoviesContainer = document.querySelector('.popular-movies-container')
+const topRatedMoviesContainer = document.querySelector('.trending-movies-container')
+
+const getData = async (url, cb) => {
   const get = {
     method: 'GET',
     headers: {
@@ -12,7 +15,25 @@ const getData = async (url, callback) => {
   try {
     const response = await fetch(url, get);
     const data = await response.json();
-    callback(data);
+    cb(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const postData = async (url,cb) => {
+  const post = {
+    method:"POST",
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${apiToken}`
+    }
+  }
+
+  try {
+    const response = await fetch(url, get);
+    const data = await response.json();
+    cb(data);
   } catch (err) {
     console.error(err);
   }
@@ -21,16 +42,38 @@ const getData = async (url, callback) => {
 // Popular movies API call
 const popularMovies = () => {
   getData(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${apiKey}`, (data) => {
-    console.log(data);
+    data.results.forEach((movie) => {
+      popularMoviesContainer.innerHTML += `
+        <div class="movie-card">
+        <h1 class="title">${movie.original_title}</h1>
+        <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="movie-img" class="movie-img">
+        <p class="overview">${movie.overview}</p>
+        <p class="score">Rate:${movie.vote_average}</p>
+        </div>
+      `;
+    });
   });
 };
+
+
 
 // Top rated movies API call
 const topRatedMovies = () => {
   getData(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${apiKey}`, (data) => {
-    console.log(data);
+    data.results.forEach((movie) => {
+      topRatedMoviesContainer.innerHTML += `
+        <div class="movie-card">
+        <h1 class="title">${movie.original_title}</h1>
+        <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="movie-img" class="movie-img">
+        <p class="overview">${movie.overview}</p>
+        <p class="score">Rate:${movie.vote_average}</p>
+        </div>
+      `;
+    });
   });
 };
 
 popularMovies();
 topRatedMovies();
+
+

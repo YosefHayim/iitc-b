a977adbb7b9ee7e314bef7c9a971d7cec41c6c42
Yosef Hayim <yosefisabag+03@gmail.com>
2024-10-16 02:56:04 +0300

    fds

--- October 2024/14.10.2024/Sukot assigment/functionalities/main.js
+++ October 2024/14.10.2024/Sukot assigment/functionalities/main.js
@@ -1,7 +1,10 @@
 import { apiKey, apiToken } from "./env.js";
 
 const popularMoviesContainer = document.querySelector('.popular-movies-container')
-const topRatedMoviesContainer = document.querySelector('.trending-movies-container')
+const topRatedMoviesContainer = document.querySelector('.top-trending-movies-container')
+const searchResultContainer = document.querySelector('.search-results-container')
+const formData = document.querySelector('form');
+
 
 const getData = async (url, cb) => {
   const get = {
@@ -25,11 +28,10 @@ const getData = async (url, cb) => {
 const popularMovies = () => {
   getData(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${apiKey}`, (data) => {
     data.results.forEach((movie) => {
-      // Create the movie card div
+
       const movieCardDiv = document.createElement('div');
       movieCardDiv.classList.add('movie-card');
       
-      // Determine the correct star rating image based on vote_average
       let starRatingImage;
       if (movie.vote_average < 2) {
         starRatingImage = "../images/one-star-icon.svg";
@@ -43,7 +45,6 @@ const popularMovies = () => {
         starRatingImage = "../images/five-stars-icon.svg";
       }
 
-      // Set the inner HTML of the movie card
       movieCardDiv.innerHTML = `
         <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="movie-img" class="movie-img">
         <h1 class="title">${movie.original_title}</h1>
@@ -58,32 +59,12 @@ const popularMovies = () => {
       `;
       
 
-      // Append the movie card to the container
       popularMoviesContainer.appendChild(movieCardDiv);
 
-      // Call getMoviesTrailers and pass the movie card div
       getMoviesTrailers(movie.id, movieCardDiv);
     });
   });
 };
-
-const getMoviesTrailers = (movieId, movieCardDiv) => {
-  // Locate the play button within the movie card
-  const playButton = movieCardDiv.querySelector('.play-button-btn');
-  
-  getData(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&append_to_response=videos`, (data) => {
-    // Use the first available trailer
-    const movieTrailer = data.videos.results[0];
-    if (movieTrailer) {
-      // Set the click event to open the trailer link in the current tab
-      playButton.addEventListener('click', () => {
-        window.location.href = `https://www.youtube.com/watch?v=${movieTrailer.key}`;
-      });
-    }
-  });
-};
-
-
 
 // Top rated movies API call
 const topRatedMovies = () => {
@@ -123,8 +104,96 @@ const topRatedMovies = () => {
   });
 };
 
+const getMoviesTrailers = (movieId, movieCardDiv) => {
+  // Locate the play button within the movie card
+  const playButton = movieCardDiv.querySelector('.play-button-btn');
+  
+  getData(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&append_to_response=videos`, (data) => {
+    
+    // Use the first available trailer
+    const movieTrailer = data.videos.results[0];
+    if (movieTrailer) {
+      // Set the click event to open the trailer link in the current tab
+      playButton.addEventListener('click', () => {
+        window.location.href = `https://www.youtube.com/watch?v=${movieTrailer.key}`;
+      });
+    }
+  });
+};
+
+
+const searchMovies = () => {
+  formData.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    const inputValue = formData.querySelector('input').value.trim();
+    
+    if(inputValue.length === 0 || inputValue.length < 2) {
+      alert('Please provide a movie name')
+    } else {
+
+      popularMoviesContainer.style.display = `none`;
+      topRatedMoviesContainer.style.display = `none`;
+
+      if (searchResultContainer.children.length > 1) {
+        searchResultContainer.innerHTML = ``
+      }
+      
+      getData(`https://api.themoviedb.org/3/search/movie?query=${inputValue}&include_adult=false&language=en-US&page=1`, (data) => {
+        if (data.results.length === 0) {
+          window.location.href = 'error404.html';
+        } else {
+          data.results.forEach((movie) => {
+
+            // Determine the correct star rating image based on vote_average
+            let starRatingImage;
+            if (movie.vote_average < 2) {
+              starRatingImage = "../images/one-star-icon.svg";
+            } else if (movie.vote_average < 4) {
+              starRatingImage = "../images/two-stars-icon.svg";
+            } else if (movie.vote_average < 6) {
+              starRatingImage = "../images/three-stars-icon.svg";
+            } else if (movie.vote_average < 8) {
+              starRatingImage = "../images/four-stars-icon.svg";
+            } else {
+              starRatingImage = "../images/five-stars-icon.svg";
+            }
+            
+            searchResultContainer.innerHTML += `
+            <div class="movie-card">
+            <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="movie-img" class="movie-img">
+            <h1 class="title">${movie.original_title}</h1>
+            
+            <div class="img-container">
+            <img src="${starRatingImage}" alt="rating-img" class="rating-img">
+            <h2 class="rating-number-txt">${movie.vote_average.toFixed(1)}</h2>
+            
+            </div>
+            <div class="overview-container">
+            <p class="overview">${movie.overview}</p>
+            </div>
+            </div>
+            `
+          })
+        }})
+      }
+    }
+    )
+  }
+
+const resetPlaceholder = () => {
+  const searchInput = document.querySelector('.input-search-bar');
+  
+  searchInput.addEventListener('focus', () => {
+    searchInput.placeholder = ''; 
+  });
+  
+  searchInput.addEventListener('blur', () => {
+    searchInput.placeholder = 'Search movies'; 
+  });
+}
+
 
 popularMovies();
 topRatedMovies();
-
-
+resetPlaceholder()
+searchMovies()

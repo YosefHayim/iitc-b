dcd7728867a4400b9ccaad28ede8a500b8df9bba
Yosef Hayim <yosefisabag+03@gmail.com>
2024-10-26 17:42:58 +0300

    fdsfdsfdsfdsdffsdfsd

--- October 2024/14.10.2024/Sukot assigment/functionalities/dom/redirect-to-404-dom.js
+++ October 2024/14.10.2024/Sukot assigment/functionalities/dom/redirect-to-404-dom.js
@@ -3,7 +3,7 @@ const redirectToErrorPage = () => {
   window.location.href = 'error404.html';
   setTimeout(() => {
     window.location.href = 'index.html';
-  }, 4000);
+  }, 3000);
 };
 
 export { redirectToErrorPage };

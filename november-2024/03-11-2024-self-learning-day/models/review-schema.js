2fe84bff67578e6351f839477e9d028ee137dc88
Yosef Hayim <yosefisabag+03@gmail.com>
2024-11-03 21:31:30 +0200

    fds

--- november-2024/03-11-2024-self-learning-day/models/review-schema.js
+++ november-2024/03-11-2024-self-learning-day/models/review-schema.js
@@ -10,13 +10,16 @@ const reviewSchema = new mongoose.Schema(
       default: 0,
       validate: {
         validator: function (value) {
-          return value > 1 && value < 5;
+          return value >= 1 && value <= 5;
         },
-        message: (props) => `${props.value} Please provide valid rating 1 - 5.`,
+        message: (props) =>
+          `${props.value} is invalid. Please provide a rating between 1 and 5.`,
       },
     },
     comment: {
       type: String,
+      minlength: [1, "Comment must be at least 1 character long."],
+      maxlength: [50, "Comment cannot exceed 50 characters."],
     },
     book: {
       type: mongoose.Schema.Types.ObjectId,

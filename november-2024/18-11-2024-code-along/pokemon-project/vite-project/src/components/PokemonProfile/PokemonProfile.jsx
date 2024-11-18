ac3e2b7c235ef349da4147ddf0dc6c862a0f3d77
Yosef Hayim <yosefisabag+03@gmail.com>
2024-11-18 18:37:00 +0200

    fsd

--- november-2024/18-11-2024-code-along/pokemon-project/vite-project/src/components/PokemonProfile/PokemonProfile.jsx
+++ november-2024/18-11-2024-code-along/pokemon-project/vite-project/src/components/PokemonProfile/PokemonProfile.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/prop-types */
 import axios from "axios";
 import { useState, useEffect } from "react";
+import styles from "./PokemonProfile.module.css";
 
 const PokemonProfile = ({ pokemonUrl }) => {
   const [pokemon, setPokemon] = useState(null);
@@ -18,19 +20,44 @@ const PokemonProfile = ({ pokemonUrl }) => {
   return (
     pokemon && (
       <div>
-        <div>
-          <h1>Name: {pokemon.name}</h1>
+        <div key={pokemon.name} className={styles.PokemonCard}>
+          <h1>{pokemon.name} Card Viewer</h1>
           <h2>ID: {pokemon.id}</h2>
-          <h2>Type: {pokemon.types[0].type.name}</h2>
-          <div className="ImgContainer">
+
+          <div className={styles.TypesContainer}>
+            <h2>Types:</h2>
+            <ul>
+              {pokemon.types.map(({ type }, index) => (
+                <li key={index}>{type.name}</li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.ImgContainer}>
             <img
               src={pokemon.sprites.other.dream_world.front_default}
               alt="Pokemon Creature"
             />
+          </div>
+          <div className={styles.weight}>
+            <h2>Pokemon Weight: {pokemon.weight}</h2>
+          </div>
+          <div className={styles.BaseExperienceContainer}>
+            <h2>Experience: {pokemon.base_experience}</h2>
+          </div>
+          <div className={styles.AbilitiesContainer}>
             <h2>Abilities:</h2>
             <ul>
               {pokemon.abilities.map(({ ability }, index) => (
                 <li key={index}>{ability.name}</li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.StatsContainer}>
+            <ul>
+              {pokemon.stats.map(({ base_stat, stat }, index) => (
+                <li key={index}>
+                  {stat.name.toUpperCase()}: {base_stat}
+                </li>
               ))}
             </ul>
           </div>

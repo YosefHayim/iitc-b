6fec49372dade95d83e50729f9a568aab8f11e9a
Yosef Hayim <yosefisabag+03@gmail.com>
2024-11-21 22:36:27 +0200

    dsa

--- november-2024/18-11-2024-code-along/pokemon-project/vite-project/src/pages/SearchForPokemon/SearchForPokemon.jsx
+++ november-2024/18-11-2024-code-along/pokemon-project/vite-project/src/pages/SearchForPokemon/SearchForPokemon.jsx
@@ -2,8 +2,12 @@ import styles from "./SearchForPokemon.module.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Loading from "../../components/Loading/Loading";
+import { useParams } from "react-router-dom";
 
 const SearchForPokemon = ({ input }) => {
+  const params = useParams();
+  console.log(params);
+
   const [pokemonUrl, setPokemonUrl] = useState("");
   const [loading, setLoading] = useState(false);
 
@@ -27,7 +31,39 @@ const SearchForPokemon = ({ input }) => {
 
   return (
     <div>
-      <h1>Pokemon Result:{loading ? <Loading /> : console.log(pokemonUrl)}</h1>
+      {loading && <Loading />}
+      {pokemonUrl && (
+        <>
+          <h2>Meet {pokemonUrl.name}</h2>
+          <img
+            src={pokemonUrl.sprites.other.dream_world.front_default}
+            alt={pokemonUrl.name}
+          />
+          <p>Weight: {pokemonUrl.weight}</p>
+          <p>Height: {pokemonUrl.height}</p>
+          <p>Base Experience: {pokemonUrl.base_experience}</p>
+          <h3>Abilities:</h3>
+          <div>
+            {pokemonUrl.abilities.map((ability, index) => (
+              <div key={index}>
+                {ability.ability.name} {ability.is_hidden && "(Hidden Ability)"}
+              </div>
+            ))}
+          </div>
+          <h3>Sprites:</h3>
+          <h3>Types:</h3>
+          <ul>
+            {pokemonUrl.types.map((type, index) => (
+              <li key={index}>{type.type.name}</li>
+            ))}
+          </ul>
+          <h3>Voice:</h3>
+          <audio controls>
+            <source src={pokemonUrl.cries.latest} type="audio/ogg" />
+            Your browser does not support the audio element.
+          </audio>
+        </>
+      )}
     </div>
   );
 };

b388c218c245d924dec2aa621b4275af37b8e5a7
Yosef Hayim <yosefisabag+03@gmail.com>
2024-11-23 12:53:07 +0200

    dsa

--- november-2024/18-11-2024-code-along/pokemon-project/vite-project/src/pages/AboutUs/AboutUs.jsx
+++ november-2024/18-11-2024-code-along/pokemon-project/vite-project/src/pages/AboutUs/AboutUs.jsx
@@ -4,33 +4,53 @@ const AboutUs = () => {
   return (
     <div>
       <h1 className={styles.AboutUsTitle}>About Us</h1>
-      <div className={styles.AboutUsContainer}>
+      <div className={styles.Introduction}>
         <p>
           Welcome to our learning journey! We're on a mission to master the art
           of full-stack development, one React component at a time.
         </p>
-        <h2>Our Mission</h2>
-        <p>
-          To build engaging web apps while leveling up our skills in React,
-          React Router, and seamless API integrations.
-        </p>
-        <h2>Potential</h2>
-        <p>
-          By conquering these tools, we're unlocking the doors to crafting
-          dynamic, user-friendly applications and advancing toward full-stack
-          expertise.
-        </p>
-        <h2>Skills We're Gaining</h2>
-        <ul>
-          <li>Building interactive components with React</li>
-          <li>Creating smooth navigation using React Router</li>
-          <li>Fetching and managing data with API calls</li>
-          <li>Designing modular and maintainable code</li>
-        </ul>
-        <p>
-          Join us on this adventure as we transform lines of code into
-          real-world solutions!
-        </p>
+      </div>
+      <div className={styles.AboutUsContainer}>
+        <div className={styles.Mission}>
+          <h2 className={styles.OurMissionTitle}>Our Mission</h2>
+          <p>
+            To build engaging web apps while leveling up our skills in React,
+            React Router, and seamless API integrations.
+          </p>
+        </div>
+        <div className={styles.Potential}>
+          <h2 className={styles.PotentialTitle}>Potential</h2>
+          <p>
+            By conquering these tools, we're unlocking the doors to crafting
+            dynamic, user-friendly applications and advancing toward full-stack
+            expertise.
+          </p>
+        </div>
+        <div className={styles.Skills}>
+          <h2 className={styles.SkillsWereGainingTitle}>
+            Skills We're Gaining
+          </h2>
+          <ul className={styles.SkillsUl}>
+            <li>Creating smooth navigation using React Router</li>
+            <li>Building interactive components with React</li>
+            <li>Fetching and managing data with API calls</li>
+            <li>Designing modular and maintainable code</li>
+          </ul>
+        </div>
+        <div className={styles.CreativeCopyright}>
+          <h2 className={styles.OurCodingJourneyTitle}>Our Coding Journey</h2>
+          <p>
+            Every line of code we write tells a story. Every error we fix, a
+            lesson learned. We're shaping the future, one keystroke at a time.
+            Thanks for being part of this epic coding adventure!
+          </p>
+        </div>
+        <div className={styles.Adventure}>
+          <p>
+            Join us on this adventure as we transform lines of code into
+            real-world solutions!
+          </p>
+        </div>
       </div>
     </div>
   );

471f91eee3d65f206891048458d9ce81887f2d32
YosefHayim <yosefisabag+03@gmail.com>
2024-11-26 12:02:24 +0200

    dsa

--- november-2024/26-11-2024-redux-code-along/vite-project/src/App.jsx
+++ november-2024/26-11-2024-redux-code-along/vite-project/src/App.jsx
@@ -1,25 +1,17 @@
-import { useState } from "react";
-
 import "./App.css";
+import { Provider } from "react-redux";
+import { store } from "./store/index.js";
+import TodoList from "./components/TodoList.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <Provider store={store} />
+      <div>
+        <h2>React To Do List</h2>
+        <TodoList />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Provider />
     </>
   );
 }

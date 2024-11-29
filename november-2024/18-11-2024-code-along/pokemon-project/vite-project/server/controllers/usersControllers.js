ebfc3a263f397bc490fbe5634f63f84eb4ea0668
Yosef Hayim <yosefisabag+03@gmail.com>
2024-11-29 13:56:07 +0200

    fsd

---
+++ november-2024/18-11-2024-code-along/pokemon-project/vite-project/server/controllers/usersControllers.js
@@ -0,0 +1,27 @@
+const isFalsy = require("../utils/isFalsy");
+const userSchema = require("../models/userSchema");
+
+const getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await userSchema.find();
+
+    if (isFalsy(allUsers)) {
+      return res.status(404).json({ message: "No users found." });
+    }
+
+    res.status(200).json({
+      message: "Success",
+      response: allUsers,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to retrieve users.",
+      error: error.message,
+    });
+    console.error("Error retrieving users:", error);
+  }
+};
+
+module.exports = {
+  getAllUsers,
+};
--- november-2024/18-11-2024-code-along/pokemon-project/vite-project/server/routes/usersRoute.js
+++ november-2024/18-11-2024-code-along/pokemon-project/vite-project/server/routes/usersRoute.js
@@ -1,28 +1,8 @@
 const express = require("express");
-const isFalsy = require("../utils/isFalsy");
-const userSchema = require("../models/userSchema");
+const getAllUsers = require("../controllers/usersControllers");
 
 const router = express.Router();
 
-router.get("/all", async (req, res) => {
-  try {
-    const allUsers = await userSchema.find();
-
-    if (isFalsy(allUsers)) {
-      return res.status(404).json({ message: "No users found." });
-    }
-
-    res.status(200).json({
-      message: "Success",
-      response: allUsers,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Failed to retrieve users.",
-      error: error.message,
-    });
-    console.error("Error retrieving users:", error);
-  }
-});
+router.get("/all", getAllUsers);
 
 module.exports = router;

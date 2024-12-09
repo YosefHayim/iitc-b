53006afba69a103e359f02542c607b1dc3a0354d
yosefhayim <yosefisabag+03@gmail.com>
2024-12-09 16:39:50 +0200

    ds

---
+++ december-2024/09-12-2024-code-along-typescript/src/App.tsx
@@ -0,0 +1,69 @@
+import { useRef, useState } from "react";
+
+interface Todo {
+  id: string;
+  text: string;
+  completed: boolean;
+  description?: string;
+  [key: string]: any;
+}
+
+function App() {
+  const textInputRef = useRef<HTMLInputElement | null>(null);
+  const descriptionInputRef = useRef<HTMLInputElement | null>(null);
+  const [dummyList, setDummyList] = useState<Todo[]>([
+    {
+      id: "1",
+      text: "text 1",
+      completed: false,
+      description: "this is description text 1",
+    },
+    {
+      id: "2",
+      text: "text 2",
+      completed: false,
+      description: "this is description text 2",
+    },
+  ]);
+
+  const handleClick = () => {
+    const textInput = textInputRef.current?.value;
+    const description = descriptionInputRef.current?.value;
+    if (textInput && description) {
+      setDummyList((prev) => [
+        ...prev,
+        {
+          id: Date.now().toString(),
+          text: textInput,
+          completed: false,
+          description,
+        },
+      ]);
+    }
+  };
+
+  return (
+    <div className="flex flex-row items-center justify-center w-full bg-red-500">
+      <h1>Learn Typescript TODO Exercise</h1>
+      {dummyList.map((obj) => (
+        <div key={obj.id}>
+          <p>ID: {obj.id}</p>
+          <p>Description: {obj.description}</p>
+          <p>Text: {obj.text}</p>
+          <p>Completed: {obj.completed}</p>
+        </div>
+      ))}
+      <div>
+        <label htmlFor="">Text Field</label>
+        <input type="text" ref={textInputRef} />
+      </div>
+      <div>
+        <label htmlFor="">Description Field</label>
+        <input type="text" ref={descriptionInputRef} />
+      </div>
+      <button onClick={handleClick}>Add todo</button>
+    </div>
+  );
+}
+
+export default App;

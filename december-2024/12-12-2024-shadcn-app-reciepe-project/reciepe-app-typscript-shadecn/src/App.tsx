4d81a702289188705ac05a9479e653c444417636
YosefHayim <yosefisabag+03@gmail.com>
2024-12-12 15:07:15 +0200

    fds

--- december-2024/12-12-2024-shadcn-app-reciepe-project/reciepe-app-typscript-shadecn/src/App.tsx
+++ december-2024/12-12-2024-shadcn-app-reciepe-project/reciepe-app-typscript-shadecn/src/App.tsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Recipe from "./components/Recipe/Recipe";
+import Settings from "./components/Settings/Settings";
 
 function App() {
-  return <div>
-    
-  </div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route path="settings" element={<Settings />} />
+          <Route path="recipe/:id" element={<Recipe />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ december-2024/12-12-2024-shadcn-app-reciepe-project/reciepe-app-typscript-shadecn/src/components/Home/Home.tsx
@@ -0,0 +1,49 @@
+import placeholderImg from "../../../public/recipe-page-clone.svg";
+import { Button } from "../ui/button";
+import { MailIcon } from "../ui/Mail";
+import { FcGoogle } from "react-icons/fc";
+import { FaApple } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const imgSize = "h-[10vh] rounded-[0.5em]";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col p-[1em] font-Halenoir">
+      <div className="flex flex-row flex-wrap w-full items-center justify-between gap-[0.5em]">
+        <img src={placeholderImg} alt="" className={imgSize} />
+        <img src={placeholderImg} alt="" className={imgSize} />
+        <img src={placeholderImg} alt="" className={imgSize} />
+        <img src={placeholderImg} alt="" className={imgSize} />
+        <img src={placeholderImg} alt="" className={imgSize} />
+        <img src={placeholderImg} alt="" className={imgSize} />
+      </div>
+
+      <div>
+        <h1 className="text-center mt-[1em] mb-[1em]">
+          <b>Elevate your home cooking with our expertly curated recipes</b>
+        </h1>
+        <div className="flex flex-col w-full text-center gap-[1em]">
+          <Button variant="outline">
+            <MailIcon /> Sign up with email
+          </Button>
+          <p className="text-gray-500">or use social media</p>
+          <Button variant="outline">
+            <FcGoogle />
+            Sign up with google
+          </Button>
+          <Button>
+            <FaApple /> Sign up with apple
+          </Button>
+          <Link to="/sign-in">
+            <p className="text-gray-500">
+              Already have an account? <b className="text-black">Sign in</b>
+            </p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

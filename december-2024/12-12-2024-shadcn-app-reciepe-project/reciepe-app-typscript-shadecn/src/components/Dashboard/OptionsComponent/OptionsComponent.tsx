f2376a897e12fdf95392c7a90a88d68217432719
yosefhayim <yosefisabag+03@gmail.com>
2024-12-14 12:00:10 +0200

    fd

--- december-2024/12-12-2024-shadcn-app-reciepe-project/reciepe-app-typscript-shadecn/src/components/Dashboard/OptionsComponent/OptionsComponent.tsx
+++ december-2024/12-12-2024-shadcn-app-reciepe-project/reciepe-app-typscript-shadecn/src/components/Dashboard/OptionsComponent/OptionsComponent.tsx
@@ -4,7 +4,7 @@ const boldText = "font-bold";
 
 const OptionsComponent = ({ name }) => {
   return (
-    <div className="mb-[2.5em] flex flex-row justify-between">
+    <div className="flex flex-row justify-between">
       <h2 className={boldText}>{name}</h2>
       <div className="flex flex-row items-center justify-center">
         <button className="text-gray-500">Sell all</button>
--- december-2024/12-12-2024-shadcn-app-reciepe-project/reciepe-app-typscript-shadecn/src/components/Dashboard/Searchbar/Searchbar.tsx
+++ december-2024/12-12-2024-shadcn-app-reciepe-project/reciepe-app-typscript-shadecn/src/components/Dashboard/Searchbar/Searchbar.tsx
@@ -6,7 +6,10 @@ const Searchbar = () => {
   return (
     <div className="relative w-full flex flex-row items-center justify-between gap-[1em]">
       <IoSearchOutline className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-      <Input className="pl-10" placeholder="Search your recipes" />
+      <Input
+        className="pl-10 rounded-[100em]"
+        placeholder="Search for recipes..."
+      />
       <div>
         <FaUserCircle className="text-4xl" />
       </div>

595bb75bc64111bd2a9260eb802a1ff11c1709fc
yosefhayim <yosefisabag+03@gmail.com>
2024-12-16 15:34:33 +0200

    fd

--- december-2024/16-12-2024-socket-io/client/src/App.tsx
+++ december-2024/16-12-2024-socket-io/client/src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import "./App.css";
 import Chat from "./components/Chat/Chat";
 import Rooms from "./components/Rooms/Rooms";
+import io from "socket.io-client";
+
+const socket = io("http//localhost:3000");
 
 function App() {
   const [roomName, setRoomName] = useState<string>("");
@@ -19,6 +22,9 @@ function App() {
   return (
     <div onClick={handleClick}>
       <h1>Learn Socket.io</h1>
+      <div>
+        <h2>Messages displayed here:</h2>
+      </div>
       <Rooms />
       <Chat roomName={roomName} />
     </div>
--- december-2024/16-12-2024-socket-io/server/app.js
+++ december-2024/16-12-2024-socket-io/server/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 const PORT = process.env.PORT || 3000;
+
+app.use(cors, {
+  origin: "http://localhost:5137",
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Server is running, ready for learning socket.io");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
 });
 
 server.listen(PORT, () => {

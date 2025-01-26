5a7563eebca4a9439aa738ff6ab71da19697b768
YosefHayim <yosefisabag+03@gmail.com>
2025-01-26 14:18:17 +0200

    כגד

---
+++ automation-2025/22-01-2025/main.py
@@ -0,0 +1,6 @@
+# list 
+fav_movie_list = ['dead-pool and wolverine']
+
+# tuple
+my_birth = (17,07,2000)
+
---
+++ automation-2025/26-01-2025/lists.py
@@ -0,0 +1,39 @@
+# ex 1
+# list_of_colors = ['red','green','blue','yellow','orange']
+# print(list_of_colors[0])
+# print(list_of_colors[-1])
+
+# ex 2
+# list_of_colors.append('black')
+# list_of_colors.pop(2)
+# print(list_of_colors)
+
+# ex 3
+# even_numbers = [1,2,3,4,5,6,7,8,9,10]
+# for i in even_numbers:
+#   if i % 2 == 0:
+#     print(i)
+
+# ex 4
+# list = ['apple','fruits','banana']
+# reverse_list = list[::-1]
+# print(reverse_list)
+
+# ex 5
+# count_5_list = [5,654,6,23,1,4,5,6,7,65,5]
+# total_five_appear = 0
+# for i in count_5_list:
+#   if i == 5:
+#     total_five_appear += 1
+
+
+# ex 6
+# one_list = [1,2]
+# two_list = [3,4]
+# three_list = one_list + two_list
+# print(three_list)
+
+# ex 7
+# messy_list = [6,4,3,76,8,1,2,7,9,10,21,32]
+# print(sorted(messy_list))
+

b94a0074eddbda9d68a664180116630f97508e40
YosefHayim <yosefisabag+03@gmail.com>
2025-01-27 08:52:13 +0200

    fsd

---
+++ automation-2025/26-01-2025/dictionaries.py
@@ -0,0 +1,42 @@
+#ex 11 
+# personal_data = {
+#   "name": 'joseph',
+#   "age":10,
+#   "grade":"IITC"
+# }
+# print(personal_data["name"])
+
+#ex 12
+# personal_data['email'] = 'thisNewEmail@gmail.com'
+# print(personal_data['email'])
+
+# ex 13
+# personal_data.pop('grade')
+
+# ex 14
+# dict = {
+#   'a': 1,
+#   'b': 2,
+#   'c': 3
+# }
+
+# ex 15
+# for k, v in list(dict.items()):
+#     print(k, v)
+#     if k != 'd':
+#         dict['d'] = 4
+# print(dict)
+
+
+# ex 16
+# d_o = {
+#   'x':10
+# }
+
+# d_t = {
+#   'y':20,
+# }
+
+# d_o.update(d_t)
+# print(d_o)
+

2caa5dd37cc8d0d494590db8d1c93a360c885132
YosefHayim <yosefisabag+03@gmail.com>
2025-01-28 15:12:17 +0200

    dsa

---
+++ automation-2025/28-01-2025-selenium/example.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+# service = Service(executable_path="./chromedriver.exe")
+# options = webdriver.ChromeOptions()
+# driver = webdriver.Chrome(service=service, options=options)
+# driver.get("https://atid.store/")
+# time.sleep(10000)
+
+# driver.refresh
+# driver.back()
+# driver.forward()
+
+# dummy web for automation practice https://atid.store/
--- automation-2025/28-01-2025-selenium/main.py
+++ automation-2025/28-01-2025-selenium/main.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
+from selenium.webdriver.common.by import By
 
 service = Service(executable_path="./chromedriver.exe")
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
-driver.get("https://www.google.com")
+driver.get("https://practicetestautomation.com/practice-test-login/")
 
+username = driver.find_element(By.ID, "username").send_keys("student")
+password = driver.find_element(By.ID, "password").send_keys("Password123")
+submit_btn = driver.find_element(By.ID,'submit').click()
+print('finish')
 time.sleep(10000)

47cf6073b33f877cdb536fed363b4d31cf985326
YosefHayim <yosefisabag+03@gmail.com>
2025-01-28 15:52:27 +0200

    fd

--- automation-2025/28-01-2025-selenium/main.py
+++ automation-2025/28-01-2025-selenium/main.py
@@ -11,5 +11,13 @@ driver.get("https://practicetestautomation.com/practice-test-login/")
 username = driver.find_element(By.ID, "username").send_keys("student")
 password = driver.find_element(By.ID, "password").send_keys("Password123")
 submit_btn = driver.find_element(By.ID,'submit').click()
-print('finish')
+success_message = driver.find_element(By.XPATH,'/html/body/div/div/section/div/div/article/div[1]/h1')
+logout_button = driver.find_element(By.XPATH,'/html/body/div/div/section/div/div/article/div[2]/div/div/div/a')
+if success_message and logout_button:
+  print(f'found success message',success_message)
+print('test case 1')
+
+logout_button.click()
+
 time.sleep(10000)
+driver.quit

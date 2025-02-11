dba68ae65d8c70d3d96f5359a2939f400ac8078d
YosefHayim <yosefisabag+03@gmail.com>
2025-02-11 11:17:50 +0200

    עכ

--- automation-2025/february-2025/11-02-2025-pytest/tests/test_github_title.py
+++ automation-2025/february-2025/11-02-2025-pytest/tests/test_github_title.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
+    # waiting stage until the use of driver finish in other fns.
     yield driver
     driver.quit()
 
@@ -14,6 +15,7 @@ def test_github_title(driver):
     driver.get("https://www.github.com")
     assert "GitHub" in driver.title
 
+# passing to the parameters the additional search terms to be passed in the array to be written. which is like a loop of testing 3 times.
 @pytest.mark.parametrize("search_term", ["python", "selenium", "pytest"])
 def test_google_search(driver, search_term):
     driver.get("https://www.google.com")

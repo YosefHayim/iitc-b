d280161aca0481a5bb7f4f1b7f374f5392957652
YosefHayim <yosefisabag+03@gmail.com>
2025-02-12 11:43:53 +0200

    fd

---
+++ automation-2025/february-2025/12-02-2025-classes-pytest/pages/dashboard_page.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class DashboardPage():
+    def __init__(self  ,driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver,10)
+        self.submit_btn = (By.TAG_NAME,'button')
+
+    def check_title(self,driver,title):
+        return driver.title == title
+
+    def check_url(self,driver,current_url):
+        return driver.url == current_url
+    
---
+++ automation-2025/february-2025/12-02-2025-classes-pytest/tests/test_dashboard.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from pages.login_page import LoginPage
+from pages.dashboard_page import DashboardPage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture
+def driver():
+  driver = webdriver.Chrome()
+  yield driver
+  driver.quit()
+
+  def test_dashboard(driver):
+    login_page = LoginPage(driver)
+    login_page.access_page()
+    login_page.enter_username('testuser')
+    login_page.enter_password('password123')
+    login_page.submit_login()
+
+    dashboard_page = DashboardPage(driver)
+    dashboard_page.check_title(driver.title,'Dashboard')
+    dashboard_page.check_url(driver.url,'http://127.0.0.1:5500/client/dashboard.html')
